f82bef4b43baee0aa77ba58c928ab65218217723
Immad Hamid <immad.hamid@gmail.com>
2018-05-10 17:38:25 +0500

    -  Added prototype for Post type request - Implemented the usage in app.js using fake API - Showed the result in console

--- app.js
+++ app.js
@@ -1,25 +1,45 @@
 const http = new easyHttp;
 
-// Get Posts
-http.get('https://jsonplaceholder.typicode.com/posts', function(res) {
-    console.log(res);               // stringified form
-    console.log(JSON.parse(res));   // parsed form
-    addResponseToView(res);
+// Get Data
+http.get('https://jsonplaceholder.typicode.com/posts', function(err, res) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(res);               // stringified form
+        console.log(JSON.parse(res));   // parsed form
+        // addResponseToView(res);              Here's how you can send the response to a function and do something there
+    }
 });
+
+// Create Data
+const data = {
+    title: "Custom Post",
+    body: "Custom Post Body"
+}
+
+// Post Data
+http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, res) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(res);               // stringified form
+        console.log(JSON.parse(res));   // parsed form
+    }
+})
 
 
 // adding post data to div in an asyn way (GET)
-function addResponseToView(res) {
-    const post = JSON.parse(res);
+// function addResponseToView(res) {
+//     const post = JSON.parse(res);
 
-    post.forEach(function(post) {
-        const postDiv = document.getElementById('posts');
-        postDiv.innerHTML += `
-            <ul>
-                <li>${post.id}</li>
-                <li>${post.title}</li>
-                <li>${post.body}</li>
-            </ul>
-        `
-    });
-}
+//     post.forEach(function(post) {
+//         const postDiv = document.getElementById('posts');
+//         postDiv.innerHTML += `
+//             <ul>
+//                 <li>${post.id}</li>
+//                 <li>${post.title}</li>
+//                 <li>${post.body}</li>
+//             </ul>
+//         `
+//     });
+// }
--- easyHttp.js
+++ easyHttp.js
@@ -2,19 +2,40 @@ function easyHttp() {
     this.http = new XMLHttpRequest;
 }
 
-// prototype for a get request
+// prototype for a GET request
 easyHttp.prototype.get = function(url, callBack) {
+    // defining our request type and if its async or not
     this.http.open('GET', url, true);
 
+    // setting this to a variable to that it could be used inside the onload method
     let self = this;
-    
     this.http.onload = function() {
-
+        // checking if the status is ok or 200
         if (self.http.status === 200) {
-            callBack(this.responseText);
+            // sending the null as an error and 2nd pram as a response
+            callBack(null, self.http.responseText);
         }
+        // else showing the error with status
         else callBack('An Erorr Occured: ' + self.http.status);
     };
 
     this.http.send();
+}
+
+// prototype for a POST request
+easyHttp.prototype.post = function(url, data, callBack) {
+    // defining our request type and if its async or not
+    this.http.open('POST', url, true);
+
+    // setting headers
+    this.http.setRequestHeader('Content-type', 'application/json')
+
+    // setting this to a variable to that it could be used inside the onload method
+    let self = this;
+    this.http.onload = function() {
+        // sending the null as an error and 2nd pram as a response
+        callBack(null, self.http.responseText);
+    };
+
+    this.http.send(JSON.stringify(data));
 }
